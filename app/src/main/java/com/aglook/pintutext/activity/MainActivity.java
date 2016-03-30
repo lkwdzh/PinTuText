6fe6def6408f5db30a9610584d4f9192d3531ee1
lkwdzh <lkwdxyx@163.com>
2016-03-30 17:18:19 +0800

    Initial commit

---
+++ app/src/main/java/com/aglook/pintutext/activity/MainActivity.java
@@ -0,0 +1,66 @@
+package com.aglook.pintutext.activity;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.aglook.pintutext.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    //Temp照片路径
+    public static String TEMP_IMAGE_PATH;
+    private GridView mGvPicList;
+    // 主页图片资源ID
+    private int[] mResPicId;
+    private List<Bitmap>mPicList=new ArrayList<>();
+    private TextView mTvPuzzleMainTypeSelected;
+    private LayoutInflater mLayoutInflater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TEMP_IMAGE_PATH= Environment.getExternalStorageDirectory().getPath()+"/temp.png";
+
+    }
+
+    /*初始化Views*/
+    private void initViews(){
+        mGvPicList = (GridView) findViewById(R.id.gv_xpuzzle_main_pic_list);
+        // 初始化Bitmap数据
+        mResPicId = new int[]{
+                R.drawable.pic1, R.drawable.pic2, R.drawable.pic3,
+                R.drawable.pic4, R.drawable.pic5, R.drawable.pic6,
+                R.drawable.pic7, R.drawable.pic8, R.drawable.pic9,
+                R.drawable.pic10, R.drawable.pic11, R.drawable.pic12,
+                R.drawable.pic13, R.drawable.pic14,
+                R.drawable.pic15, R.mipmap.ic_launcher};
+        Bitmap[] bitmaps = new Bitmap[mResPicId.length];
+        for (int i = 0; i < bitmaps.length; i++) {
+            bitmaps[i]= BitmapFactory.decodeResource(getResources(),mResPicId[i]);
+            mPicList.add(bitmaps[i]);
+        }
+
+        //显示Type
+        mTvPuzzleMainTypeSelected = (TextView) findViewById(R.id.tv_puzzle_main_type_selected);
+        mLayoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
